0f633d4e872132dcdf7ab947a7811da6eb1fdf00
Benerick Juan Rivera Quiñonez <ben@Benericks-Mac-mini.local>
2025-12-09 15:41:18 -0500

    avance y laboratorios

---
+++ 001-Constants_Variables.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+// 31 fundamentals
+
+// 32 Constant
+let name = "Swift"
+print("Hello, \(name)!")
+
+// 33 Variable
+var age = 29
+print("First: \(age)!")
+age = 30
+print("Second: \(age)!")
+
+// 36 naming const/var
+//      camel case = 1ra minus, 2da Mayus... ; edadUsuarioTienda
+//      comments = //... ; more lines = /* ... */
+//      Types = Int, Double, Bool and String
+//          Collection Types = Array, Dictionary
+
+//              struct Person {                                             type person
+//                  let firstName: String                                   properties
+//                  let lastName: String                                    properties
+//              func sayHello() {                                           method
+//                  print("Hello!")
+//              }
+
+//          let aPerson = Person(fistName: "Andre", lastName: "Lorico")     Instance of person type
+//          aPerson.sayHello()
+
+// 41 typeSafety
+//      underscores = 1_000_233_721
+//      typeInference = automatic type declare
+//      or
+//      declare type (: ... =)
+//          let cityName: String = "San Francisco"
+
+// 46 lab variables and constants
